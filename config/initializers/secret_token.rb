8e702712c9e4e32f1fd0759a83ab46e91ecefb20
neil patel <neil@myindianweddingapp.com>
2013-08-23 11:00:20 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'cf7f9996df00d870cea1cf307a59636554cf7ece7b581bd8dea65f90f1e16f0b31066bfecd2a33475266fba2ae55045e8ac1345a450f5755efe9a2bc830268e7'
